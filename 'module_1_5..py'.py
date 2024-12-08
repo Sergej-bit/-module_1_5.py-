52bc9e1bd27429b2e07bafe1672720b6bde382ef
Sergej-bit <khalyavkin.s@yandex.ru>
2024-12-08 11:43:35 +0300

    Add files via upload

---
+++ 'module_1_5..py'.py
@@ -0,0 +1,8 @@
+immutable_var = ([1, 2, 3, 4], 'coconate', 'cool')
+print(immutable_var)
+immutable_var_ = (['banana','apple'], 3, 4, 'cool')
+print('Незаменяемй объек "Кортеж":', immutable_var_)
+mutable_list = ([6, 4, 7], 4)
+print(mutable_list)
+mutable_list [0] [0] = 5
+print(mutable_list)
